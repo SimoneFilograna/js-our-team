70b4a6c95e9eb11420a586cb76bbe183b67e90f4
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-07-14 15:15:27 +0200

    fixed-release-v2

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,45 +2,43 @@
 
 const boxListWorker = document.querySelector(".box");
 
-
-
 //array di oggetti con dati
 
 const workList = [
     {
-        nome: "Wayne Barnett",
-        ruolo: "Founder & CEO",
-        foto: "wayne-barnett-founder-ceo.jpg"
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        photo: "wayne-barnett-founder-ceo.jpg"
     },
     
     {
-        nome: "Angela Caroll",
-        ruolo: "Chief Editor",
-        foto: "angela-caroll-chief-editor.jpg"
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        photo: "angela-caroll-chief-editor.jpg"
     },
 
     {
-        nome: "Walter Gordon",
-        ruolo: "Office Manager",
-        foto: "walter-gordon-office-manager.jpg"
+        name: "Walter Gordon",
+        role: "Office Manager",
+        photo: "walter-gordon-office-manager.jpg"
     },
 
     {
-        nome: "Angela Lopez",
-        ruolo: "Social Media Manager",
-        foto: "angela-lopez-social-media-manager.jpg"
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        photo: "angela-lopez-social-media-manager.jpg"
     },
 
     {
-        nome: "Scott Estrada",
-        ruolo: "Developer",
-        foto: "scott-estrada-developer.jpg"
+        name: "Scott Estrada",
+        role: "Developer",
+        photo: "scott-estrada-developer.jpg"
     },
 
     {
-        nome: "Barbara Ramos",
-        ruolo: "Graphic Designer",
-        foto: "barbara-ramos-graphic-designer.jpg"
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        photo: "barbara-ramos-graphic-designer.jpg"
     },
 ] 
 
@@ -48,11 +46,11 @@ const workList = [
 
 for(let i = 0; i < workList.length; i++){
     console.log(workList[i]);   
-    const singleWorker = workList[i];
+    let singleWorker = workList[i];
 
     //stampo ogni singola proprietà dell'oggetto e stampo le foto (bonus - 1 )
 
-    boxListWorker.innerHTML += `<p>${singleWorker.nome} ${singleWorker.ruolo} <img src="./assets/img/${singleWorker.foto}"></p>`
+    boxListWorker.innerHTML += `<p>${singleWorker.name} ${singleWorker.role} <img src="./assets/img/${singleWorker.photo}"></p>`
 }
 
 
